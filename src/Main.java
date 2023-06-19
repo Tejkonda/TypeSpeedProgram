741503b54334197bae64cd6d325a8704cdc54a16
DELL <kondaveetitejaswi99@gmail.com>
2023-06-18 21:26:23 -0500

    Created a staring position and generated a paragraph.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException  {
+        RandomTexts R= new RandomTexts();
+        System.out.println("Let's");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Get");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Start The Game");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Type the following lines");
+        System.out.println(R.RandomParagraph());
+
+
+
     }
 }
--- src/RandomTexts.java
+++ src/RandomTexts.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RandomTexts {
+
+import java.util.Random;
+
+public class RandomTexts {
+    String Basketball = " The game of basketball came in the year 1891, \n" +
+            "in Springfield, Massachusetts. Many other sports formed an inspiration for basketball.\n " +
+            "It has features of soccer and rugby. ";
+    String Cricket ="Cricket is a sport that requires the use of a bat and ball " +
+            "Cricket is particularly famous in England and India. ";
+
+    public String RandomParagraph(){
+        String[] paragraphs = {Basketball, Cricket};
+        Random random = new Random();
+        int randomIndex = random.nextInt(paragraphs.length);
+        return paragraphs[randomIndex];
+
+    }
 }
